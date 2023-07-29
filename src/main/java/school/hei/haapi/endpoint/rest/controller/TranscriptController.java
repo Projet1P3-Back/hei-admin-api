d856fbd55d73cbe9ce01ab2d22f29d24feeef402
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-07-29 17:56:57 +0300

    fixe: fixing bug and delete some file

--- src/main/java/school/hei/haapi/endpoint/rest/controller/TranscriptController.java
+++ src/main/java/school/hei/haapi/endpoint/rest/controller/TranscriptController.java
@@ -5,26 +5,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import school.hei.haapi.endpoint.rest.mapper.TranscriptMapper;
+import school.hei.haapi.endpoint.rest.model.Transcript;
 import school.hei.haapi.model.BoundedPageSize;
 import school.hei.haapi.model.PageFromOne;
-import school.hei.haapi.model.Transcript;
 import school.hei.haapi.service.TranscriptService;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
 
 @RestController
 @AllArgsConstructor
 public class TranscriptController {
     private final TranscriptService transcriptService;
-    @GetMapping(value = "/students/{id}/transcripts")
-    public List<Transcript> getTranscript(
-            @RequestParam PageFromOne page, @RequestParam("page_size") BoundedPageSize pageSize,
-            @RequestParam(value = "studentId", required = false, defaultValue = "") String studentId
+    private final TranscriptMapper transcriptMapper;
+    @GetMapping(value = "/students/{studentId}/transcripts")
+    public List<school.hei.haapi.endpoint.rest.model.Transcript> getTranscripts(
+            @PathVariable String studentId,
+            @RequestParam PageFromOne page, @RequestParam("page_size") BoundedPageSize pageSize
+
             ){
-        return transcriptService.getByStudentId(studentId, page, pageSize);
+        return transcriptService.getTranscriptByStudentId(studentId, page, pageSize)
+                .stream()
+                .map(transcriptMapper::toRest).collect(toUnmodifiableList());
     }
-    @GetMapping(value = "/students/{id}/transcripts/{id}")
-    public  Transcript getTranscriptId(@PathVariable String id){
-        return transcriptService.getTranscriptById(id);
+    @GetMapping(value = "/students/{studentId}/transcripts/{transcriptId}")
+    public Transcript getTranscriptId(@PathVariable String studentId,@PathVariable String transcriptId){
+        return  transcriptMapper.toRest(transcriptService.getByStudentIdAndId(studentId, transcriptId));
     }
 }
--- src/main/java/school/hei/haapi/repository/TranscriptVersionRepository.java
+++
@@ -1,7 +0,0 @@
-package school.hei.haapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.hei.haapi.model.Version;
-
-public interface TranscriptVersionRepository extends JpaRepository<Version,String> {
-}
