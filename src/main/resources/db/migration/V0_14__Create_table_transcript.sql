da74345a0e1c5d3a02b22a6aebde7529353867c3
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-07-29 17:43:39 +0300

    feat: adding  scriptsql for transcript

--- src/main/resources/db/migration/V0_14__Create_table_transcript.sql
+++ src/main/resources/db/migration/V0_14__Create_table_transcript.sql
@@ -1,10 +1,19 @@
+
+do
+$$
+    begin
+        if not exists(select from pg_type where typname = 'semester') then
+            create type semester as enum ('s1', 's2', 's3', 's4');
+        end if;
+    end
+$$;
 create table if not exists "transcript"
 (
     id              varchar
         constraint transcript_pk        primary key default  uuid_generate_v4(),
     student_id      varchar
         constraint student_id_fk             references "user" (id),
-    semester        varchar,
+    semester        semester,
     academicYear    varchar,
     isDefinitive    varchar,
     creationDatetime timestamp with time zone not null default now()
